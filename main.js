0517750c4ab234640c08f86d2607005f24cd3039
Joaquin Mayer <joakinmayer96@gmail.com>
2020-03-10 00:28:51 -0300

    Computed Propierties

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: "Vue Mastery",
         product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
+        selectedVariant: 0,
         anchor: "#",
-        inStock: true,
         details: ["80% cotton", "20% polyester", "Gender-natural"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: './assets/vmSocks-green-onWhite.jpg'
+                variantImage: './assets/vmSocks-green-onWhite.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: './assets/vmSocks-blue-onWhite.jpg'
+                variantImage: './assets/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0
             }
         ],
         cart: 0
@@ -24,8 +26,19 @@ var app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
     }
 })
